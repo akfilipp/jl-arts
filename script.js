1cfd5038ee31ac007d02f5953ee3fd1c55b4983c
alison filippini <alison@MacBook-Air.attlocal.net>
2022-10-23 14:33:48 -0400

    created send message form

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let searchBtn = document.getElementById("search-btn");
+// FORM VALIDATION PRACTICE
+const email = document.getElementById("email");
+const msgForm = document.getElementById("send-msg");
+const emailErrMsg = document.getElementById("email-err");
 
-searchBtn.addEventListener("click", function() {
-    console.log('Searching . . .');
-})
+email.addEventListener ('input', function(e) {
+    const regexEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    const currentValue = e.target.value;
+    const valid = regexEmail.test(currentValue); 
+
+    if (valid) {
+        emailErrMsg.style.display = 'none'
+    }
+    else {
+        emailErrMsg.style.display = 'block'
+    }
+});
+
