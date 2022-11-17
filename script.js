10cf416cc7f0f587016263b538017935c9956d77
alison filippini <alison@MacBook-Air.attlocal.net>
2022-11-17 11:33:04 -0500

    adjusted img file paths

--- script.js
+++ script.js
@@ -77,34 +77,6 @@ rightButton.addEventListener("click", function() {
   }
 })
 
-// GALLERY SLIDER with 'Tiny Slider' Library
-// let slider = tns({
-//     container : ".my-slider",
-//     "slideBy" : 1,
-//     "speed": 400,
-//     "nav": false,
-//     controlsContainer: "#controls",
-//     prevButton : ".previous",
-//     nextButton : ".next",
-//     responsive: {
-//         1600: {
-//             items: 4,
-//             gutter: 20
-//         },
-//         1024: {
-//             items: 3,
-//             gutter: 20
-//         },
-//         768: {
-//             items: 2,
-//             gutter: 20
-//         },
-//         480: {
-//             items: 1
-//         }
-//     }
-// });
-
 // FORM VALIDATION
 const email = document.getElementById("email");
 const msgForm = document.getElementById("send-msg");
